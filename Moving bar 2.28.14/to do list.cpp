834843534cb50404dd6446cb6dc3652baa941b48
Chris Jang <chrismangggg@gmail.com>
2014-03-01 00:37:11 -0600

    added to do list

---
+++ Moving bar 2.28.14/to do list.cpp
@@ -0,0 +1,15 @@
+/*
+To do list
+==========
+IMMEDIATE:
+-> implement red flash for when AI scores a point
+-> scorekeeping graphics
+-> fix collisions (moar collision physics)
+
+FUTURE:
+-> menu
+-> actual AI
+-> music
+-> sounds
+-> DLC
+*/

cdcc337cb5ac43a4885625b57166e7abd7073cba
Chris Jang <chrismangggg@gmail.com>
2014-03-01 00:34:08 -0600

    Got a lot of shit done. need to work on more collision physics, point mechanics, scorekeeping, menu, etc, etc

--- Moving bar 2.28.14/main.cpp
+++ Moving bar 2.28.14/main.cpp
@@ -4,19 +4,29 @@
 #include <stdio.h>
 
 //window dimensions
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 //sdl variables
 SDL_Window* mainWindow = NULL;
 SDL_Renderer* globalRenderer = NULL;
 
+//assets
 //the bars
 SDL_Texture* leftBar = NULL;
 SDL_Texture* rightBar = NULL;
-int leftBarRectPosY = 0;
+float leftBarRectPosX = 0.0;
+float leftBarRectPosY = 0.0;
+float rightBarRectPosX = SCREEN_WIDTH - 100;
 float rightBarRectPosY = 0.0;
-bool movingUp = false; //determine whether the AI should be moving up or down
+
+bool barMovingUp = false; //determine whether the AI should be moving up or down
+//ball
+SDL_Texture* ball = NULL;
+float ballPosX = (SCREEN_WIDTH / 2) - 25;
+float ballPosY = (SCREEN_HEIGHT / 2) - 25;
+bool ballMovingRight = false;
+bool ballMovingUp = false;
 
 //forward declarations
 bool init();
@@ -80,6 +90,9 @@ bool loadMedia() {
 	//right bar image
 	rightBar = loadTexture("assets/bar_right.png");
 
+	//ball
+	ball = loadTexture("assets/ball.png");
+
 	if (leftBar == NULL) {
 		printf("Failed to load 'bar_left' image!\n");
 		success = false;
@@ -87,6 +100,11 @@ bool loadMedia() {
 
 	if (rightBar == NULL) {
 		printf("Failed to load 'bar_right' image!\n");
+		success = false;
+	}
+
+	if (ball == NULL) {
+		printf("Failed to load 'ball' image!\n");
 		success = false;
 	}
 
@@ -124,6 +142,8 @@ void close() {
 	leftBar = NULL;
 	SDL_DestroyTexture(rightBar);
 	rightBar = NULL;
+	SDL_DestroyTexture(ball);
+	ball = NULL;
 
 	//destroy renderer
 	SDL_DestroyRenderer(globalRenderer);
@@ -136,6 +156,37 @@ void close() {
 	//quit
 	IMG_Quit();
 	SDL_Quit();
+}
+
+//method for finding out if a bar is present in a certain coordinate
+bool isBarPresent(float x, float y) {
+	if (x <= leftBarRectPosX + 100 && (y >= leftBarRectPosY && y <= leftBarRectPosY + 299)) {
+		return true;
+	}
+	else if (x + 50 >= rightBarRectPosX && (y >= rightBarRectPosY && y <= rightBarRectPosY + 299)) {
+		return true;
+	}
+
+	return false;
+}
+
+//reset function for after a point is scored
+void reset() {
+	ballPosX = (SCREEN_WIDTH / 2) - 25;
+	ballPosY = (SCREEN_HEIGHT / 2) - 25;
+}
+
+//method to update the score (0 = you, 1 = AI, 2 = reset)
+void updateScore(int player) {
+	SDL_Rect screenRect = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+	for (float i = 255.0; i >= 0.0; i -= 0.5) {
+		SDL_RenderSetViewport(globalRenderer, &screenRect);
+		SDL_SetRenderDrawColor(globalRenderer, 0, i, 0, 0);
+		SDL_RenderFillRect(globalRenderer, &screenRect);
+		SDL_RenderPresent(globalRenderer);
+	}
+
+	reset();
 }
 
 int main(int argc, char* args[]) {
@@ -184,7 +235,7 @@ int main(int argc, char* args[]) {
 
 				//render to dimensions of the left bar
 				SDL_Rect leftBarRect;
-				leftBarRect.x = 0;
+				leftBarRect.x = leftBarRectPosX;
 				leftBarRect.y = leftBarRectPosY;
 				leftBarRect.w = 100;
 				leftBarRect.h = 300;
@@ -203,7 +254,7 @@ int main(int argc, char* args[]) {
 
 				//rect for the AI bar
 				SDL_Rect rightBarRect;
-				rightBarRect.x = SCREEN_WIDTH - 100;
+				rightBarRect.x = rightBarRectPosX;
 				rightBarRect.y = rightBarRectPosY;
 				rightBarRect.w = 100;
 				rightBarRect.h = 300;
@@ -214,22 +265,71 @@ int main(int argc, char* args[]) {
 				//dont let the AI bar go off the screen
 				if (rightBarRect.y < 0) {
 					rightBarRectPosY = 0;
-					movingUp = false;
+					barMovingUp = false;
 				}
 				if (rightBarRect.y > SCREEN_HEIGHT - 300) {
 					rightBarRectPosY = SCREEN_HEIGHT - 300;
-					movingUp = true;
+					barMovingUp = true;
 				}
 
 				//draw the AI image
 				SDL_RenderCopy(globalRenderer, rightBar, NULL, NULL);
 
 				//increment the ai bar so it keeps moving
-				if (movingUp) {
-					rightBarRectPosY -= 0.01;
+				if (barMovingUp) {
+					rightBarRectPosY -= 0.05;
 				}
 				else {
-					rightBarRectPosY += 0.01;
+					rightBarRectPosY += 0.05;
+				}
+
+				//ball movement
+				SDL_Rect ballRect;
+				ballRect.x = ballPosX;
+				ballRect.y = ballPosY;
+				ballRect.w = 50;
+				ballRect.h = 50;
+
+				//set viewport to ballRect
+				SDL_RenderSetViewport(globalRenderer, &ballRect);
+
+				//collision physics
+				if (isBarPresent(ballPosX, ballPosY)) {
+					//change directions (left/right)
+					if (ballMovingRight)
+						ballMovingRight = false;
+					else
+						ballMovingRight = true;
+				}
+				if (ballPosY < 0.0) {
+					ballMovingUp = false;
+				}
+				else if ((ballPosY + 50) > SCREEN_HEIGHT) {
+					ballMovingUp = true;
+				}
+				
+				//WIN CONDITIONS
+				if (ballPosX < 0.0)
+					updateScore(1); //give opponent a point
+				else if (ballPosX + 50 > SCREEN_WIDTH)
+					updateScore(0); //give yourself a point
+
+				//draw the ball
+				SDL_RenderCopy(globalRenderer, ball, NULL, NULL);
+
+				//determine how the ball will move
+				if (ballMovingRight) {
+					ballPosX += 0.05;
+				}
+				else {
+					ballPosX -= 0.05;
+				}
+
+				if (ballMovingUp) {
+					ballPosY -= 0.05;
+				}
+				else {
+					ballPosY += 0.05;
 				}
 
 				//update the screen
